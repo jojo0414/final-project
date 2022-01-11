55c39b4dc9b2ee0a0d65cd0b4894dc6d9d139005
jojo <jojo22123770@gmail.com>
2022-01-11 03:56:16 -0800

    Initial Commit

---
+++ final project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  final project
+//
+//  Created by 陳昕喬🐷 on 2022/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ final project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final project
+//
+//  Created by 陳昕喬🐷 on 2022/1/11.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
